c75d346027fe727aa2b2193d7355bfc2e5fbe0bb
RashikaNagan <149616337+RashikaNagan@users.noreply.github.com>
2024-10-22 18:25:15 +0200

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__ (self, age, grade,):
+        self.age = age
+        self.grade = grade
+    
+    def birthyear(self):
+        output = 2024-self.age
+        print(output)
+        
+
+Yara = Student(14, 9)
+
+Yara.birthyear()
